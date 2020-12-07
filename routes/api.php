fc0d8e51a93734ad464027d434231e0bb07614ff
Mohammad Masoudi <mohammad.masoudi59@gmail.com>
2020-12-08 00:05:38 +0330

    add getDoctor route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\API\DoctorSpecialtyController;
 use App\Http\Controllers\API\DoctorAvailableDaysController;
 use App\Models\Doctor;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,62 +33,66 @@ use App\Models\User;
 // });
 
 // Route::group(['middleware' => 'api'], function () {
-Route::get('test', function(){
+Route::get('test', function () {
     return response(['message' => 'user not auth'], 401);
 })->name('verify');
-    Route::group(['prefix' => 'auth'], function () {
-        Route::post('login', [AuthController::class, 'login']);
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
-        Route::post('register', [RegisterController::class, 'register']);
-        Route::post('doctor/register', [DoctorRegisterController::class, 'register']);
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('register', [RegisterController::class, 'register']);
+    Route::post('doctor/register', [DoctorRegisterController::class, 'register']);
+});
+Route::group(['middleware' => 'jwt.verify'], function () {
+    Route::get('getDoctors', function () {
+        $doctor = DB::table('users')
+            ->join('doctors', 'users.id', '=', 'doctors.user_id')
+            ->get(['users.id','users.name', 'users.family', 'users.phone','doctors.is_active']);
+            return $doctor;
     });
-    Route::group(['middleware' => 'jwt.verify'], function () {
+    Route::get('getUser', function () {
+        $user = auth('api')->user();
 
-        Route::get('getUser', function(){
-            $user = auth('api')->user();
-
-            $doctor = Doctor::find($user->id);
-            $user = User::find($user->id);
-            if($doctor != null){
-                $doctorProfile = $doctor->profile;
-            }else{
-                $doctorProfile = null;
-            }
-            if($user != null){
-                $userProfile = $user->profile;
-            }else{
-                $userProfile = null;
-            }
-            // return $doctor;
-            
-            return response([
-                'user' => [
-                    'user' => $user,
-                    // 'profile' =>  $doctor !== null ?  $doctorProfile : $userProfile
-                ],
-                'is_doctor' => $doctor ? true : false,
-            ], 200);
-        });
-        
-        Route::group(['prefix' => 'doctor'], function () {
-            Route::resource('profile', DoctorProfileController::class);
-            Route::apiResources([
-                'specialty' => DoctorSpecialtyController::class,
-                'day' => DoctorAvailableDaysController::class,
-            ]);
-            Route::get('days' , function(){
-                return Week::all();
-            });
-            Route::get('specialites' , function(){
-                return Specialty::all();
-            });
-    
-        });
-        Route::group(['prefix' => 'user'], function () {
-            Route::post('profile', [UserProfileController::class, 'storeOrUpdate']);
-            Route::get('save-turn', [MakeTurnController::class, 'addTurn']);
-        }); 
+        $doctor = Doctor::select('id')->where('user_id', $user->id)->get();
+        // $user = User::find($user->id);
+        // if($doctor != null){
+        //     $doctorProfile = $doctor->profile;
+        // }else{
+        //     $doctorProfile = null;
+        // }
+        // if($user != null){
+        //     $userProfile = $user->profile;
+        // }else{
+        //     $userProfile = null;
+        // }
+        // return $doctor;
+        $u = User::find($user->id)->with('profile')->first();
+        return response([
+            'user' => [
+                'user' => $u,
+                // 'profile' =>  $doctor !== null ?  $doctorProfile : $userProfile
+            ],
+            'is_doctor' => $doctor->count() != 0 ? true : false,
+        ], 200);
     });
+
+    Route::group(['prefix' => 'doctor'], function () {
+        Route::resource('profile', DoctorProfileController::class);
+        Route::apiResources([
+            'specialty' => DoctorSpecialtyController::class,
+            'day' => DoctorAvailableDaysController::class,
+        ]);
+        Route::get('days', function () {
+            return Week::all();
+        });
+        Route::get('specialites', function () {
+            return Specialty::all();
+        });
+    });
+    Route::group(['prefix' => 'user'], function () {
+        Route::post('profile', [UserProfileController::class, 'storeOrUpdate']);
+        Route::get('save-turn', [MakeTurnController::class, 'addTurn']);
+    });
+});
     
 // });
